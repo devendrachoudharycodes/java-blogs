0f2b440710516d72597a807fb478c8eb6ca9bbfe
devendrachoudharycodes <devendra.choudhary.codes@gmail.com>
2026-07-05 23:04:58 +0530

    fix: font family not found error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,35 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   // Replace with your actual GitHub username
-  site: 'https://your-github-username.github.io', 
+  site: 'https://devendrachoudharycodes.github.io', 
   
   // Replace with your repository name. 
   // Note: Remove the 'base' line entirely if your repo is exactly named 'your-github-username.github.io'
-  base: '/your-repo-name', 
+  base: 'java-blogs',
+	integrations: [mdx(), sitemap()],
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: 'Atkinson',
+			cssVariable: '--font-atkinson',
+			fallbacks: ['sans-serif'],
+			options: {
+				variants: [
+					{
+						src: ['./src/assets/fonts/atkinson-regular.woff'],
+						weight: 400,
+						style: 'normal',
+						display: 'swap',
+					},
+					{
+						src: ['./src/assets/fonts/atkinson-bold.woff'],
+						weight: 700,
+						style: 'normal',
+						display: 'swap',
+					},
+				],
+			},
+		},
+	],
+
 });
